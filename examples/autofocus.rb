4d929f42c2304a48549db380f173f404c9642909
Michael Macias <zaeleus@gmail.com>
2016-02-27 01:16:37 -0600

    Add autofocus example

---
+++ examples/autofocus.rb
@@ -0,0 +1,20 @@
+require 'gphoto2'
+
+# Capture an image only after successfully autofocusing.
+#
+# Typically, if the camera fails to autofocus, updating the `autofocusdrive`
+# key will throw an "Unspecified error (-1)". This catches the exception and
+# continues reautofocusing until it is successful.
+
+GPhoto2::Camera.first do |camera|
+  begin
+    camera.update(autofocusdrive: true)
+  rescue RuntimeError
+    camera.reload
+    retry
+  ensure
+    camera.update(autofocusdrive: false)
+  end
+
+  camera.capture
+end
