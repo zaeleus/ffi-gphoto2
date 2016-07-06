e17a5b9ff6548aa517abdc5afe97a7e41fab19ea
Sijawusz Pur Rahnama <sija@sija.pl>
2016-07-06 13:04:14 +0200

    Turn tree-view into flat list

--- examples/list_files.rb
+++ examples/list_files.rb
@@ -1,17 +1,15 @@
 require 'gphoto2'
 
-# List all files in tree form.
+# List all files
 
-def visit(folder, level = 0)
-  indent = '  ' * level
-
+def visit(folder)
   files = folder.files
-  puts "#{indent}#{folder.root? ? "/ (root)" : folder.path} (#{files.size} files)"
+  puts "#{folder.root? ? "/ (root)" : folder.path} (#{files.size} files)"
 
-  folder.folders.each { |child| visit(child, level + 1) }
-  indent << '  '
+  folder.folders.each { |child| visit(child) }
+  files.each { |file| puts file.name }
 
-  puts "#{indent}#{files.map(&:name).join(' , ')}"
+  puts
 end
 
 GPhoto2::Camera.first do |camera|
