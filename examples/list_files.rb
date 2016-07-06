44e6a4c81a60e3317a1b7f70970a5e930d5a551a
Sijawusz Pur Rahnama <sija@sija.pl>
2016-07-06 22:38:41 +0200

    Put folder traversal call at the end

--- examples/list_files.rb
+++ examples/list_files.rb
@@ -6,10 +6,10 @@ def visit(folder)
   files = folder.files
   puts "#{folder.root? ? "/ (root)" : folder.path} (#{files.size} files)"
 
-  folder.folders.each { |child| visit(child) }
   files.each { |file| puts file.name }
-
   puts
+
+  folder.folders.each { |child| visit(child) }
 end
 
 GPhoto2::Camera.first do |camera|
